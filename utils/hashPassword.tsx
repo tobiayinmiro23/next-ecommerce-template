80cf3dc5616a4c90e02639547cfd90ba17e51c59
tobiayinmiro23 <87906572+tobiayinmiro23@users.noreply.github.com>
2024-09-28 15:21:00 +0100

    Delete utils/hashPassword.tsx

--- utils/hashPassword.tsx
+++
@@ -1,6 +0,0 @@
-// import bcrypt from 'bcrypt'
-// const hashPassword =async(password:string)=> {
-//     var salt = bcrypt.genSaltSync(10);
-//     var hash = bcrypt.hashSync(password, salt);
-//     return hash
-// }
